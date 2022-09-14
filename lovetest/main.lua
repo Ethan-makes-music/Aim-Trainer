068c59336ce75329d858119699ac6604a54abb88
Elicxer <94811149+Ethan-makes-music@users.noreply.github.com>
2022-09-13 21:28:05 -0400

    Add files via upload

--- lovetest/main.lua
+++ lovetest/main.lua
@@ -1,5 +1,5 @@
 function love.load()
-    love.window.setTitle("Aim trainer v0.0.1")
+    love.window.setTitle("Aim trainer v0.1.0")
     target = {}
     target.x = 200
     target.y = 400
@@ -14,7 +14,7 @@ function love.load()
 end
 
 function love.draw()
-    --sets the color and draws the shape / coloe of your choice
+    --sets the color and draws the shape / color of your choice
     love.graphics.setColor(51/255, 153/255, 255/255)
     love.graphics.circle("fill", target.x, target.y, target.radius)
 
@@ -22,18 +22,54 @@ function love.draw()
     love.graphics.setFont(gameFont)
     love.graphics.print(score, 0, 0)
     love.graphics.print(timer, 200, -0)
-    if timerDone == true and timer == 0 then 
-        love.graphics.print("Your score is: " ..score, 400,300)
-    end
 end
 
 function love.update(dt)
-    if timer <= 0 then
+    if timer <= 0 then --when the timer hits zero it will wait 5 seconds then restart the game
       timerDone = true
       love.timer.sleep(5)
-      love.event.quit()
+      love.load()
     end
     timer = timer - dt
+    if love.keyboard.isDown( "r" ) then -- if you press R it resets the game
+        love.load()
+    end
+    if love.keyboard.isDown( "escape" ) then --if you press esc it closes the game
+        love.event.quit()
+    end
+
+    -- Changes the time to the number you press and also makes the target go to its origanal pos
+    if love.keyboard.isDown( "1" ) then
+        timer = 10
+        score = 0
+        target.x = 200
+        target.y = 400
+    elseif love.keyboard.isDown( "2" ) then
+        timer = 20
+        score = 0
+        target.x = 200
+        target.y = 400
+    elseif love.keyboard.isDown( "3" ) then
+        timer = 30
+        score = 0
+        target.x = 200
+        target.y = 400
+    elseif love.keyboard.isDown( "4" ) then
+        timer = 40
+        score = 0
+        target.x = 200
+        target.y = 400
+    elseif love.keyboard.isDown( "5" ) then
+        timer = 50
+        score = 0
+        target.x = 200
+        target.y = 400
+    elseif love.keyboard.isDown( "6" ) then
+        timer = 60
+        score = 0
+        target.x = 200
+        target.y = 400
+    end
 end
 
 function love.mousepressed( x, y, button, istouch, presses )
